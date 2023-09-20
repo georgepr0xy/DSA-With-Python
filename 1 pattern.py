7fa6e6b27b528c9aec81a8f08fd8025a20c9fd07
George Province <provincegeorge412@gmail.com>
2023-09-21 00:46:04 +0530

    patter making

--- 1 pattern.py
+++ 1 pattern.py
@@ -3,3 +3,14 @@ for i in range(n):
     for j in range(n):
         print("*", end  = " ")
     print()      
+
+
+def pattern(n):
+    for i in range(n):
+       for j in range(n):
+         print("*", end  = " ")
+       print()   
+
+
+pattern(5)          
+    
