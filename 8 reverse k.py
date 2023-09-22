442e227652ab23bf3ebb27b3a6769cc234f830e1
George Province <provincegeorge412@gmail.com>
2023-09-23 00:42:28 +0530

    reverse star

--- 8 reverse k.py
+++ 8 reverse k.py
@@ -1,8 +1,8 @@
 n=5
 for i in range(n):
     for j in range(i):
-      print(" ",end = " ")
-    for k in range(n+4-2*i):
-       print("*",end=" ")
+      print(" ",end = "")
+    for k in range(2*n-(2*i+1)):
+       print("*",end="")
     print()   
            
