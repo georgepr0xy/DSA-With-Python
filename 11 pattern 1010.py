4e534c269a6564ab101526ca4b5cf78dce7b9442
George Province <provincegeorge412@gmail.com>
2023-09-23 23:10:03 +0530

    1,01,101,0101

---
+++ 11 pattern 1010.py
@@ -0,0 +1,7 @@
+n = 1
+for i in range(n+1):
+    for j in range(i,2*i):
+        print(j%2,end=" ")
+        
+    print()
+         
--- rough.py
+++ rough.py
@@ -1,2 +1,2 @@
-for i in range(1):
-    print("*")
+for i in range(2, 4):
+    print(i)
