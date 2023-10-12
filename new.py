9d493ab2d3f139b7474df3e8ed3d8bd43b281373
George Province <provincegeorge412@gmail.com>
2023-10-12 23:51:42 +0530

    changes

--- new.py
+++ new.py
@@ -1,7 +1,16 @@
-n = 5
-for i in range(n):
-      for j in range(i):
-          print("*", end=" ")
-          j = j+1
-      print()
+# n = 5
+# for i in range(n):
+#       for j in range(i):
+#           print("*", end=" ")
+#           j = j+1
+#       print()
+
+n = ["abcdefghijklmnopqrstuvwxyz"]
+m= 5
+
+for i in range(m):
+    for j in n[i]:
+      print(j, end=" ")
+    
+    
           
