c5642297320f870e1f6af6e982d1fa982bcc0178
George Province <provincegeorge412@gmail.com>
2024-02-20 00:36:24 +0530

    palindrome

--- checek palindrome.py
+++ checek palindrome.py
@@ -1,6 +1,22 @@
-str = "ABCDCBA"
-p1 = 1
-p2 = len(str)-1
+# str = "ABCDCBA"
+# p1 = 1
+# p2 = len(str)-1
 
-for i in range(len(str)):
-    if arr[]
+# while p1<p2:
+#   if str[p1] == str[p2]:
+#     p1 += 1
+#     p2 -= 1
+
+#   elif:
+#      print("Its is not a palindrome")
+  
+s = "madam"
+def  palindrome(i,s):
+    if i >= len(s)//2:
+        return True
+    if s[i] != s[len(s)-1-i]:
+        return False
+    return palindrome(i+1,s)
+
+b = palindrome(0,s)
+print(b)
