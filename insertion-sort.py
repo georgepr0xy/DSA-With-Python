d975f67a2d0a4551db7df2c4b998143c490e882c
George Province <provincegeorge412@gmail.com>
2024-03-14 02:23:06 +0530

    string chai aur code

---
+++ insertion-sort.py
@@ -0,0 +1,9 @@
+arr =[44,55,33,22,66,11]
+temp = 6*[0]
+print(temp)
+
+for i in range(1,len(arr)-1):
+    if arr[0]>arr[i]:
+        temp[0]==arr[0]
+        arr[0]==arr[i]
+        
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,7 +1,4 @@
-for i in range(n+1):
-#     for j in range(i):
-#       print(" ",end = "")
-#     for k in range(n-i):
-#        print("* ",end="")
-#     print()   
-               
+for i in range(1,len(arr)-1):
+#     if arr[0]>arr[i]:
+#         ar
+#         arr[0]==arr[i]
